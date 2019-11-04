6b436f5a7b2f9315abeecd72f3480dc2d4363341
psychoticpolice <calling911x@gmail.com>
2019-11-04 11:03:30 -0500

    Added the flowchart for NodeEditor Execution

---
+++ NodeEditor/Resources/NodeBlockExceptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeEditor.Resources
+{
+	/// <summary>
+	/// this is a class that i use to hold all the custom errors for the node block editor.
+	/// </summary>
+	class NodeBlockExceptions
+	{
+	}
+}
