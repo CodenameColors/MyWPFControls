17f94569d5b71fd2b1433f8eb2a492164b23b9f5
CodenameColors <calling911x@gmail.com>
2023-03-02 23:05:27 -0500

    Added a Croppable user control for the engine!

---
+++ CroppableImage/ImageCropper/CroppableImage.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CroppingImageLibrary.Services;
+
+namespace ImageCropper
+{
+    /// <summary>
+    /// Interaction logic for CroppableImage.xaml
+    /// </summary>
+    public partial class CroppableImage : UserControl
+    {
+				public CropService CropService { get; private set; }
+				private BitmapImage _baseImage = new BitmapImage();
+
+				private bool _bHasFocus = false;
+
+				public bool bHasFocus
+				{
+					get => _bHasFocus;
+					set
+					{
+						_bHasFocus = value;
+						if(!_bHasFocus)
+						{
+							CropService?.ClearAdorners(this);
+							if(CropService == null)
+								CropService = new CropService(this);
+						}
+					}
+				}
+	
+				public CroppableImage()
+        {
+            InitializeComponent();
+            CropService = null;
+        }
+        private void RootGrid_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+	        CropService?.Adorner.RaiseEvent(e);
+        }
+
+        private void RootGrid_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+	        CropService?.Adorner.RaiseEvent(e);
+        }
+
+        private void RootGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+	        // CropService = new CropService(this);
+
+        }
+
+        public void SetImage(String bitmapImagePath, bool bChangeSize)
+        {
+	        _baseImage.BeginInit();
+	        _baseImage.UriSource = new Uri(bitmapImagePath, UriKind.Absolute);
+					_baseImage.EndInit();
+
+
+					SourceImage.Source = _baseImage;
+					if(bChangeSize)
+					{
+						if(CropService == null)
+							CropService = new CropService(this);
+
+						this.Width = _baseImage.Width;
+						this.Height = _baseImage.Height;
+						CropService.Adorner.Width = this.Width;
+						CropService.Adorner.Height = this.Height;
+						ImageBorder.Width = this.Width;
+						ImageBorder.Height = this.Height;
+
+						RootGrid.Height = _baseImage.Height;
+						RootGrid.Width = _baseImage.Width;
+
+						this.UpdateLayout();
+						CropService.ClearAdorners(this);
+						CropService = new CropService(this);
+						CropService.Adorner.UpdateLayout();
+						UpdateDefaultStyle();
+
+					}
+        }
+
+    private void CropImage_OnClick(object sender, RoutedEventArgs e)
+    {
+      CropService?.ClearAdorners(this);
+			if (CropService != null)
+				CropService = null;
+			CropService = new CropService(this);
+
+
+		}
+
+		private void ResetCropSize_OnClick(object sender, RoutedEventArgs e)
+    {
+      throw new NotImplementedException();
+    }
+
+		private void ImageBorder_OnMouseRightButtonUp(object sender, MouseButtonEventArgs e)
+		{
+			ContextMenu cm = this.FindResource("CroppableImage_CM") as ContextMenu;
+			cm.PlacementTarget = sender as Grid;
+			cm.IsOpen = true;
+		}
+
+		private void RootGrid_OnLostFocus(object sender, RoutedEventArgs e)
+		{
+			CropService?.ClearAdorners(this);
+			CropService = null;
+		}
+    }
+}
---
+++ CroppableImageTester/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CroppingImageLibrary.Services;
+using ImageCropper;
+
+namespace CroppableImageTester
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void ImportNewImage_BTN_Click(object sender, RoutedEventArgs e)
+		{
+			Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog
+			{
+				Title = "Import PNG File",
+				FileName = "", //default file name
+				Filter = "Dialogue Scene files (*.png)|*.png",
+				RestoreDirectory = true
+			};
+
+			Nullable<bool> result = dlg.ShowDialog();
+			// Process save file dialog box results
+			string filename = "";
+			if (result == true)
+			{
+				// Save document
+				filename = dlg.FileName;
+				filename = filename.Substring(0, filename.LastIndexOfAny(new Char[] { '/', '\\' }));
+			}
+			else return; //invalid name
+
+			Console.WriteLine(dlg.FileName);
+			int charcnt = 0;
+			//DIALOGUE SCENE HOOKS
+
+			CroppableImage.SetImage(dlg.FileName, true);
+
+			//Rectangle r = new Rectangle()
+			//{
+			//	Width = (int)sprite.GetPropertyData("width"),
+			//	Height = (int)sprite.GetPropertyData("height"),
+			//	Fill = new ImageBrush(img.Source)
+			//}; //Make a rectange the size of the image
+
+			//ContentControl CC = ((ContentControl)this.TryFindResource("MoveableImages_Template"));
+			//CC.Width = (int)sprite.GetPropertyData("width");
+			//CC.Height = (int)sprite.GetPropertyData("height");
+
+			//Canvas.SetLeft(CC, (int)sprite.GetPropertyData("x"));
+			//Canvas.SetTop(CC, (int)sprite.GetPropertyData("y"));
+			//Canvas.SetZIndex(CC, Zindex);
+			//Selector.SetIsSelected(CC, false);
+			//CC.MouseRightButtonDown += ContentControl_MouseLeftButtonDown;
+			//((Rectangle)CC.Content).Fill = new ImageBrush(img.Source);
+			//LevelEditor_Canvas.Children.Add(CC);
+		}
+
+		private void PrintOutAreaToLog_BTN_Click(object sender, RoutedEventArgs e)
+		{
+			CropArea ca = CroppableImage.CropService.GetCroppedArea();
+			
+			outputlog.AddErrorLogItem(0, String.Format("Area Found: Xpos:{0}, Ypos:{1} | [Width:{2}, Height:{3}]", ca.CroppedRectAbsolute.X, ca.CroppedRectAbsolute.Y,
+				ca.CroppedRectAbsolute.Width, ca.CroppedRectAbsolute.Height), "Croppable Image", false);
+		}
+
+		private void Canvas_On_Click(object sender, MouseButtonEventArgs e)
+		{
+			Canvas c = sender as Canvas;
+			if (c != null)
+			{
+				ClearAdorners(c);
+				CroppableImage.bHasFocus = false;
+			}
+		}
+
+		public void ClearAdorners(FrameworkElement adornedElements)
+		{
+			var adornerLayer = AdornerLayer.GetAdornerLayer(adornedElements);
+			var adornersOfStackPanel = adornerLayer.GetAdorners(adornedElements);
+			if (adornersOfStackPanel == null) return;
+			foreach (var adorner in adornersOfStackPanel)
+				adornerLayer.Remove(adorner);
+		}
+	}
+}
